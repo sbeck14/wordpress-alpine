42998fa6ab08fd92f270240faf60a2f62d166129
Sam Beckett <becketts@byu.edu>
2018-07-09 09:36:44 -0600

    added https:// to config vars

--- wp-config.php
+++ wp-config.php
@@ -86,8 +86,8 @@ define( 'UPLOADS', 'wp-content/uploads' );
 define( 'AS3CF_AWS_ACCESS_KEY_ID', getenv('S3_ACCESS_KEY'));
 define( 'AS3CF_AWS_SECRET_ACCESS_KEY', getenv('S3_SECRET_KEY'));
 
-define('WP_HOME','http://'. $_SERVER['SERVER_NAME']);
-define('WP_SITEURL','http://'. $_SERVER['SERVER_NAME']);
+define('WP_HOME','https://'. $_SERVER['SERVER_NAME']);
+define('WP_SITEURL','https://'. $_SERVER['SERVER_NAME']);
 
 /* That's all, stop editing! Happy blogging. */
 
